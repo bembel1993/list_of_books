981a7561ea20e3af4611e44f917fed88486ee7f6
Vitali <bembel1.20@gmail.com>
2025-02-15 19:46:29 +0300

    9 pagination 10

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -32,7 +32,7 @@ class CrudController extends Controller
 
     public function read_bk()
     {
-        $books = Book::orderBy('id', 'desc')->paginate(5);
+        $books = Book::orderBy('id', 'desc')->paginate(10);
         return view('books.index', compact('books'));
     }
 
